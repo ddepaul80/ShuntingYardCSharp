9ba9a770aef15c937e93d71e8e30d57a98415789
ddepaul80 <ddepaul80@gmail.com>
2017-09-24 18:17:13 -0400

    Add files via upload

---
+++ ShuntingYardAlgorithm/ShuntingYardAlgorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShuntingYardAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] infix = { "3 + 4", "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3", "2 * 3 - 4 / 5", "( 4 + 8 ) * ( 6 - 5 ) / ( ( 3 - 2 ) * ( 2 + 2 ) )", "( 300 + 23 ) * ( 43 - 21 ) / ( 84 + 7 )" };
+            //string[] infix = { "5 + 2 ^ 3", "2 + 1 - 12 / 3", "162 / ( 2 + 1 ) ^ 4" };
+            //string[] infix = {"4 ^ 2", "5 + 3 ^ 3" };
+            ShuntingYard sy = new ShuntingYard();
+
+            foreach (string elem in infix)
+            {
+                Console.WriteLine("Infix notation: " + elem);
+                string[] postfix = sy.ConvertInfixToPostfix(elem);
+                Console.Write("Postfix notation: ");
+                foreach (string postfixElem in postfix)
+                    Console.Write(postfixElem + " ");
+
+                Console.WriteLine();
+
+                decimal val = sy.CalculatePostfix(postfix);
+                Console.WriteLine("Result: " + val);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShuntingYardAlgorithm/ShuntingYardAlgorithm/ShuntingYard.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuntingYardAlgorithm
+{
+    class ShuntingYard
+    {
+        public string[] ConvertInfixToPostfix(string infix)
+        {
+            Queue<string> queue = new Queue<string>();
+            Stack<string> stack = new Stack<string>();
+            string[] infixArray = infix.Split(' ');
+            
+            foreach (string elem in infixArray)
+            {
+                switch (elem)
+                {
+                    case "^":
+                        stack.Push(elem);
+                        break;
+                    case "*":
+                        if (stack.Count > 0)
+                        {
+                            string op = stack.Peek();
+                            if (op == "^" || op == "/")
+                                queue.Enqueue(stack.Pop());
+                        }
+                        stack.Push(elem);
+                        break;
+                    case "/":
+                        if (stack.Count > 0)
+                        {
+                            string op = stack.Peek();
+                            if (op == "^" || op == "*")
+                                queue.Enqueue(stack.Pop());
+                        }
+                        stack.Push(elem);
+                        break;
+                    case "+":
+                        if (stack.Count > 0)
+                        {
+                            string op = stack.Peek();
+                            if (op == "^" || op == "*" || op == "/" || op == "-")
+                                queue.Enqueue(stack.Pop());
+                        }
+                        stack.Push(elem);
+                        break;
+                    case "-":
+                        if (stack.Count > 0)
+                        {
+                            string op = stack.Peek();
+                            if (op == "^" || op == "*" || op == "/" || op == "+")
+                                queue.Enqueue(stack.Pop());
+                        }
+                        stack.Push(elem);
+                        break;
+                    case "(":
+                        stack.Push(elem);
+                        break;
+                    case ")":
+                        while (stack.Peek() != "(")
+                            queue.Enqueue(stack.Pop());
+                        stack.Pop();
+                        break;
+                    default:
+                        queue.Enqueue(elem);
+                        break;
+                }
+            }
+            int count1 = stack.Count;
+            for (int i = 0; i < count1; i++)
+                queue.Enqueue(stack.Pop());
+
+            
+            int count2 = queue.Count;
+            string[] postfix = new string[count2];
+            for (int j = 0; j < count2; j++)
+                postfix[j] = queue.Dequeue();
+
+            return postfix;
+        }
+
+        public decimal CalculatePostfix(string[] postfix)
+        {
+            Stack<decimal> stack = new Stack<decimal>();
+            decimal number = decimal.Zero;
+
+            foreach (string elem in postfix)
+            {
+                if (decimal.TryParse(elem, out number))
+                    stack.Push(number);
+                else
+                {
+                    switch (elem)
+                    {
+                        case "^":
+                            number = stack.Pop();
+                            stack.Push(Exponent(stack.Pop(), number));
+                            break;
+                        case "*":
+                            stack.Push(Multiplication(stack.Pop(), stack.Pop()));
+                            break;
+                        case "/":
+                            number = stack.Pop();
+                            stack.Push(Division(stack.Pop(), number));
+                            break;
+                        case "+":
+                            stack.Push(Addition(stack.Pop(), stack.Pop()));
+                            break;
+                        case "-":
+                            number = stack.Pop();
+                            stack.Push(Subtraction(stack.Pop(), number));
+                            break;
+                        default:
+                            Console.WriteLine("I have an error");
+                            break;
+                    }
+                }
+            }
+            return stack.Pop();
+        }
+
+        private decimal Addition(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        private decimal Subtraction(decimal num1, decimal num2)
+        {
+            return num1 - num2;
+        }
+
+        private decimal Multiplication(decimal num1, decimal num2)
+        {
+            return num1 * num2;
+        }
+
+        private decimal Division(decimal num1, decimal num2)
+        {
+            return num1 / num2;
+        }
+
+        private decimal Exponent(decimal num1, decimal num2)
+        {
+            if(num2 == 0)
+                return 1;
+            else if (num2 == 1)
+                return num1;
+            else
+            {
+                decimal num = num1;
+                for (int i = 2; i <= num2; i++)
+                    num = num * num1;
+
+                return num;
+            }
+        }
+    }
+}
